f9a092729a2460a5f2218b90c7cb16ce768365e6
Kekko-dev <francesco.silvestro03@gmail.com>
2025-02-20 10:36:10 +0100

    1) Added into the info.plist the camera usage 2) Added the Content_Camera_View for see on the high level the camera usage integrated with Scanner and Coordinator 3) Added the Scanner and added the Coordinator

---
+++ TocAqui/TocAqui/TEST_CAMERA_VIEW/Content_Camera_View.swift
@@ -0,0 +1,18 @@
+//
+//  Content_Camera_View.swift
+//  TocAqui
+//
+//  Created by Francesco Silvestro on 20/02/25.
+//
+
+import SwiftUI
+
+struct Content_Camera_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Content_Camera_View()
+}
---
+++ TocAqui/TocAqui/TEST_CAMERA_VIEW/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  Scanner_View.swift
+//  TocAqui
+//
+//  Created by Francesco Silvestro on 20/02/25.
+//
+
+import SwiftUI
+
+struct Scanner_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scanner_View()
+}
